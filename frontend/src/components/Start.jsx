00a96867d6e213127f40966480ebaf670c747e3f
local <local@local.com>
2024-04-26 11:00:54 +0530

    getting a card

--- frontend/src/components/Start.jsx
+++ frontend/src/components/Start.jsx
@@ -1,12 +1,17 @@
-import React from 'react';
-import whatsup from './artifacts/whatsup.png'; 
+import React from "react";
+import whatsup from "./artifacts/whatsup.png";
 
 const Start = () => {
   return (
-    <header className="start-header">
-      <img src={whatsup} alt="Whatsup Logo" className="start-img" />
-      <h5 className="start-heading">WHATSUP WEB</h5>
-    </header>
+    <>
+      <header className="start-header">
+        <img src={whatsup} alt="Whatsup Logo" className="start-img" />
+        <h5 className="start-heading">WHATSUP WEB</h5>
+      </header>
+      <div className="start-body">
+        <div className="start-card">hello</div>
+      </div>
+    </>
   );
 };
 
