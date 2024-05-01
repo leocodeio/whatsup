2b04f6e9f6a6e99a3f79ee962ad8a9e650f25939
local <local@local.com>
2024-05-01 18:10:03 +0530

    applied css more

--- frontend/src/components/parts/Text.jsx
+++ frontend/src/components/parts/Text.jsx
@@ -1,16 +1,23 @@
 import React from "react";
+import { IoIosSend } from "react-icons/io";
+import { FaSearch } from "react-icons/fa";
 
 const Text = () => {
   return (
     <>
       <div className="text-area">
         <div className="search-area">
-          <form method="post">
+          <form method="post" className="search-sending">
             <input type="text" placeholder="#Tag here" />
-            <button type="submit">search</button>
+            <button type="submit"><FaSearch /></button>
           </form>
         </div>
-        <div className="chat-area">here chatting will be done</div>
+        <div className="chat-area">
+          <form method="post" className="message-sending">
+            <input type="text" placeholder="message..." />
+            <button type="submit"><IoIosSend /></button>
+          </form>
+        </div>
       </div>
     </>
   );
