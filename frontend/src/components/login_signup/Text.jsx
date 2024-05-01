a8c75c84d3a2335ba001cbf23c745268246174cd
local <local@local.com>
2024-05-01 12:38:17 +0530

    some prorss in seach area

--- frontend/src/components/login_signup/Text.jsx
+++ frontend/src/components/login_signup/Text.jsx
@@ -4,7 +4,12 @@ const Text = () => {
   return (
     <>
       <div className="text-area">
-        <div className="search-area">here searching will be done</div>
+        <div className="search-area">
+          <form method="post">
+            <input type="text" placeholder="#Tag here" />
+            <button type="submit">search</button>
+          </form>
+        </div>
         <div className="chat-area">here chatting will be done</div>
       </div>
     </>
