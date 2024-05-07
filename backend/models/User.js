c32038a546aab31e2f16184b699c12e72f2ec295
local <local@local.com>
2024-05-07 22:02:22 +0530

    updated models more

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,10 +1,24 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  name: String,
-  email: String,
-  password: String,
-  tag: String,
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6,
+  },
+  tag: {
+    type: String,
+    required: true,
+  },
 });
 
 const User = mongoose.model("User", userSchema);
